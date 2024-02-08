d356792a85ee16d5813dd21231cac601594474b7
Creeper76 <86135033+Creeper76@users.noreply.github.com>
2024-02-07 21:10:12 -0500

    Lots of random things

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ clock = pygame.time.Clock()
 
 # Set the variables
 drawing = False
+rainbow = False
 button = 0
 radius = 7
 pos = (0, 0)
@@ -33,22 +34,30 @@ def draw(r, isDrawing, drawCircle = False):
     # Get the current mouse position
     pos = pygame.Vector2(pygame.mouse.get_pos())
 
-    if isDrawing != 1:
+    if isDrawing != 1 and not rainbow:
       pygame.draw.circle(window, black, (int(pos.x), int(pos.y)), int(r))
       prev_pos = None
       return
+    elif isDrawing != 1 and rainbow:
+      c = get_color_from_pos(pos)
+      for x in range (int(pos.x) - r*2, int(pos.x) + r*2, 7):
+        for y in range (int(pos.y) - r*2, int(pos.y) + r*2, 7):
+          draw_with_pos(x, y, 7)
+      prev_pos = None
+      return
+    
+    if rainbow and not drawCircle and prev_pos is not None:
+      pygame.draw.line(window, white, prev_pos, pos, r)
+      pygame.draw.circle(window, white, prev_pos, r/2)
+      pygame.draw.circle(window, white, pos, r/2)
+      prev_pos = pos
+      return
+    elif rainbow and drawCircle:
+      pygame.draw.circle(window, white, pos, r/2)
+      prev_pos = pos
+      return
 
-    # Normalize the mouse position
-    norm_pos = pygame.Vector2(pos.x / window.get_width(), pos.y / window.get_height())
-
-    # Interpolate between the colors of the rainbow
-    hue = norm_pos.x * 360
-    if hue < 0:
-      hue = 0
-    saturation = 100
-    lightness = 20 + norm_pos.y * 60
-    c = pygame.Color(0)
-    c.hsla = (hue, saturation, lightness, 1)
+    c = get_color_from_pos(pos)
 
     # Draw the line
     if drawCircle:
@@ -60,13 +69,53 @@ def draw(r, isDrawing, drawCircle = False):
 
     # Update the previous mouse position
     prev_pos = pos
+    
+def draw_with_pos(x, y, r):
+  c = get_color_from_pos(pygame.Vector2(x, y))
+  pygame.draw.circle(window, c, (x, y), r)
+    
+def get_color_from_pos(pos):
+  # Normalize the mouse position
+  norm_pos = pygame.Vector2(pos.x / window.get_width(), pos.y / window.get_height())
+
+  # Interpolate between the colors of the rainbow
+  hue = norm_pos.x * 360
+  if hue < 0:
+    hue = 0
+  elif hue > 360:
+    hue = 360
+  saturation = 100
+  lightness = 20 + norm_pos.y * 60
+  c = pygame.Color(0)
+  c.hsla = (hue, saturation, lightness, 1)
+  return c
 
 # Main loop
 while True:
   for event in pygame.event.get():
+    # Handle closing the window
     if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
       pygame.quit()
       sys.exit()
+    elif event.type == pygame.KEYDOWN:
+      # C to clear the screen
+      if event.key == pygame.K_c:
+        window.fill(black)
+        rainbow = False
+      
+      # W to make the screen white
+      elif event.key == pygame.K_w:
+        window.fill(white)
+        rainbow = False
+        
+      # R to make the screen rainbow
+      elif event.key == pygame.K_r:
+        for x in range(0, width, 7):
+          for y in range(0, height, 7):
+            draw_with_pos(x, y, 7)
+        rainbow = True
+      
+    # Checks for mouse button events
     if event.type == pygame.MOUSEBUTTONDOWN:
       drawing = True
       button = event.button
@@ -74,6 +123,14 @@ while True:
         draw(radius, 1, True)
       elif event.button == 3:  # Right mouse button.
         draw(radius * 2, 0)
+        
+      # Middle scroll wheel to change the radius
+      elif event.button == 4:
+        radius += 1  # Scrolling up
+      elif event.button == 5:
+        radius -= 1  # Scrolling down
+        if radius < 1:
+          radius = 1
     elif event.type == pygame.MOUSEBUTTONUP:
       drawing = False
       prev_pos = None
@@ -91,5 +148,5 @@ while True:
 
   # Draw
   pygame.display.flip()
-  clock.tick(240)
+  clock.tick(512)
   print("FPS:", clock.get_fps())
